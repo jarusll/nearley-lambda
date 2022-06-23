d70091a18791dccfa8663180cdb01ceef059b486
Suraj Yadav <ysuraj877@gmail.com>
2022-06-23 14:39:41 +0530

    bubble up changes in main.js

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 const nearley = require('nearley')
-const grammar = require('./grammars/grammar.js')
+const grammar = require('./grammar.js')
 const evaluate = require('./eval')
 
 const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar))
 
 try {
-	parser.feed(`-1`)
-	console.log(JSON.stringify(parser.results[0], null, 4))
-	// console.log(evaluate(parser.results[0]))
+	parser.feed(`lambda X . X + 1 . 1`)
+	// console.log(JSON.stringify(parser.results[0], null, 4))
+	console.log(evaluate(parser.results[0]))
 } catch (e) {
 	console.log(e.message, e)
 }
